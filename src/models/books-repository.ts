eb99d34453ce6d88fbdb3f9f2f55809d979e1155
Verda Muso <gearmobile@gmail.com>
2024-02-14 13:13:45 +0300

    add abs class

---
+++ src/models/books-repository.ts
@@ -0,0 +1,21 @@
+export abstract class BooksRepository {
+  createBook(book): void {
+    //
+  }
+
+  getBook(id): void {
+    //
+  }
+
+  getBooks(): void {
+    //
+  }
+
+  updateBook(id): void {
+    //
+  }
+
+  deleteBook(id): void {
+    //
+  }
+}
